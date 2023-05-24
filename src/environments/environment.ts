d2cb2f97ffb12da2d8fac8b79ba800ae333e6f1e
TY_Syedhassan <syed.h@testyantra.in>
2023-05-24 10:08:25 +0530

    hosting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  baseUrl:'http://localhost:8002'
+  production: true,
+  // baseUrl:'http://localhost:8002',
+  baseUrl :'https://api.chandan.club/'
 };
 
 /*
